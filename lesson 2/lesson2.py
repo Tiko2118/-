a293ea34ffc09fed7c6805e8aa69fcf4a737954c
Tiko2118 <tinatiniomanadze@gmail.com>
2022-10-25 07:50:42 -0700

    second commit

---
+++ lesson 2/lesson2.py
@@ -0,0 +1,46 @@
+name = "tiko"
+print(len(name))
+
+
+my_sentence = "nitsa kashia dzalian kargi gogoa"
+print(my_sentence)
+
+
+knows_programming = True
+if 10>5:
+    print("hello")
+
+
+full_name = "tiko omanadze"
+print(len(full_name))      
+ 
+print(full_name[0])
+
+print("z" in full_name)
+print("s" in full_name)
+print("k" not in full_name)
+print(full_name[2:7])
+print(full_name[2:9:2])
+print(full_name[:5])
+print(full_name[5:])
+
+
+print(full_name[-1])
+print(full_name[-10])
+
+print(full_name[-4]+"abc")
+print(full_name[-1:-4:-1])
+
+print(full_name.upper())
+
+print(name.lower())
+print(name.strip())
+
+name2 = "   tiko   "
+
+
+print(name2.replace(" ", "#"))
+
+name =("tiko omanadze")
+print(name.replace("tiko omanadze", "$$$$"))
+ 
